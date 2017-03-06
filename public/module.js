f30044d34bba11a0c72efd937e022e7810b81a05
Spencer White <swhite215@live.com>
2017-03-06 10:13:43 -0500

    Added ngRouting and adjusted index.html slightly

--- public/module.js
+++ public/module.js
@@ -1,1 +1,11 @@
-var app = angular.module('myModule', []);
+var app = angular.module('myModule', ['ngRoute']);
+
+app.config(function($routeProvider, $locationProvider) {
+  $routeProvider
+    .when('/lyric', {
+      controller: 'getControl',
+      templateUrl: 'lyrics.html'
+    });
+
+    $locationProvider.hashPrefix('');
+});
