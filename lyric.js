2989041aa16e7926ce14517f22cebfce171f5174
Spencer White <swhite215@live.com>
2017-03-06 09:27:40 -0500

    Added express server and basic html

---
+++ lyric.js
@@ -0,0 +1,5 @@
+var lyrics = [
+  "It was all a dream.", "The wheels on the bus.", "I came in like a wrecking ball.", "Shake it off.", "Sitting in the sun.", "Im a joker, Im a smoker"
+];
+
+module.exports = lyrics;
